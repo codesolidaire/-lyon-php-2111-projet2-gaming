287f12f1f4247900a4f446532830e9958d2cce8b
Ticha974 <natacha.crepu@hotmail.fr>
2022-01-11 12:40:58 +0100

    Home page & update DB

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,26 +1,19 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: aurelwcs
- * Date: 08/04/19
- * Time: 18:40
- */
-
 namespace App\Controller;
+
+use App\Model\NewsManager;
 
 class HomeController extends AbstractController
 {
     /**
      * Display home page
-     *
-     * @return string
-     * @throws \Twig\Error\LoaderError
-     * @throws \Twig\Error\RuntimeError
-     * @throws \Twig\Error\SyntaxError
      */
     public function index()
     {
-        return $this->twig->render('Home/index.html.twig');
+        $newsManager = new NewsManager();
+        $news = $newsManager->selectLimited();
+        return $this->twig->render('Home/index.html.twig', ['news' => $news]);
     }
+
 }
